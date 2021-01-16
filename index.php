24bf008d3c8d6f8785aabfc7afa39c8648d2a087
Renan Ribeiro <renanrib17@bol.com.br>
2021-01-16 20:13:32 -0300

    1.0

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/css.css">
-    <title>Carápio Interativo</title>
-  </head>
 
-  <body>
-  	<div class="bg-image"></div>
-  	<center>
-	    <div class="text-center bg-text" >
-	  		<img src="img/foto.jpg" class="rounded" width="100%" alt="Logo">
-		</div>
-		<div class="bg-text-botton">
-			<center>
-			<img src="img/cardapio.png" class="cardapioimg">
-			<img src="img/whats.png" class="cardapioimg">
-			<img src="img/insta.png" class="cardapioimg">
-			</center>
-		</div>
-	</center>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  </body>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/css.css">
+  <title>Carápio Interativo</title>
+</head>
+
+<body>
+  <div class="bg-image"></div>
+  <center>
+    <div class="text-center bg-text">
+      <img src="img/foto.jpg" class="rounded" width="100%" alt="Logo">
+    </div>
+    <div class="bg-text-botton">
+      <center>
+        <img src="img/cardapio.png" class="cardapioimg">
+        <a>
+          <img src="img/whats.png" class="cardapioimg">
+        </a>
+        <img src="img/insta.png" class="cardapioimg">
+      </center>
+    </div>
+  </center>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
 </html>
 <?php
 
